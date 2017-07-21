59f6f763598d2a9fc1dd11a556a8dc6eb1ad0601
gOd <god@gods-imac.local>
2017-07-21 16:38:59 +0400

    Initial Commit

---
+++ Manhattan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Manhattan
+//
+//  Created by gOd on 7/21/17.
+//  Copyright © 2017 gOd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
